e4845680935d7ee65f979476d7bab13e62c69586
Razrodrigue <119799092+Razrodrigue@users.noreply.github.com>
2024-02-05 19:19:42 +0200

    commit

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,18 +2,63 @@
 
 import React, { useState } from 'react';
 import { IonContent, IonInput, IonLabel, IonButton, IonPage, IonHeader, IonToolbar, IonTitle, IonRouterLink} from '@ionic/react';
-import { IonIcon } from '@ionic/react';
+import { IonIcon , IonImg} from '@ionic/react';
 import { person, mail, lockClosed } from 'ionicons/icons';
+
+import {IonItem} from '@ionic/react';
+
+interface InternalValues {
+  file: any;
+}
 
 const Register: React.FC = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  // const handleRegister = () => {
-  //   // Ajoutez ici la logique d'inscription
-  //   console.log(`Register with username: ${username}, email: ${email}, and password: ${password}`);
-  // };
+ 
+  //pour upload
+  const [file, setFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const onFileChange = (fileChangeEvent: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = fileChangeEvent.target.files?.[0];
+  
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+  
+
+  const submitForm = async () => {
+    if (!file) {
+      return;
+    }
+    setLoading(true);
+
+    const formData = new FormData();
+
+    formData.append('photo', file, file.name);
+
+    try {
+      const response = await fetch('http://localhost:3000/', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('Erreur lors du téléchargement du fichier :', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -67,9 +112,17 @@ const Register: React.FC = () => {
         </IonLabel>
         <IonInput type="email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} />
         <IonLabel position="stacked">
-          <IonIcon icon={lockClosed} /> Password
+        <IonIcon icon={lockClosed} /> Password
         </IonLabel>
         <IonInput type="password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} />
+        {/* upload */}
+        <IonItem>
+          <input type="file" onChange={(ev) => onFileChange(ev)} />
+        </IonItem>
+        <IonButton color="primary" expand="full" onClick={() => submitForm()} disabled={loading}>
+          {loading ? 'Uploading...' : 'Upload'}
+        </IonButton>
+        {/* upload */}
 
         <IonButton expand="block" onClick={handleRegister}>
           Register
